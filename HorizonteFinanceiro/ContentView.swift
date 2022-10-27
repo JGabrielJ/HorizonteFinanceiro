87c2fdb5bb5b8a877e8ee79cd4bbbfb8cd0c6703
João Gabriel <gjacinto0707@gmail.com>
2022-10-27 17:01:52 -0300

    Initial Commit

---
+++ HorizonteFinanceiro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HorizonteFinanceiro
+//
+//  Created by João Gabriel Jacinto da Silva on 27/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HorizonteFinanceiro/HorizonteFinanceiroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizonteFinanceiroApp.swift
+//  HorizonteFinanceiro
+//
+//  Created by João Gabriel Jacinto da Silva on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HorizonteFinanceiroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
