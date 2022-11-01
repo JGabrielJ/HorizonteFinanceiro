9731946ea0b750387092810bf072be5b8f066e34
João Gabriel <gjacinto0707@gmail.com>
2022-11-01 14:05:56 -0300

    Create InitialView.swift

---
+++ HorizonteFinanceiro/HomeScreens/InitialView.swift
@@ -0,0 +1,21 @@
+//
+//  InitialView.swift
+//  HorizonteFinanceiro
+//
+//  Created by João Gabriel Jacinto da Silva on 01/11/22.
+//
+
+import SwiftUI
+
+
+struct InitialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InitialView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialView()
+    }
+}
