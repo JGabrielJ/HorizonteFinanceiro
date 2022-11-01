5d72204cfeaa842852b9d63fb822e4d45491ae45
João Gabriel <gjacinto0707@gmail.com>
2022-11-01 14:32:37 -0300

    Created new files

---
+++ HorizonteFinanceiro/OptionScreens/OptionView.swift
@@ -0,0 +1,20 @@
+//
+//  OptionView.swift
+//  HorizonteFinanceiro
+//
+//  Created by João Gabriel Jacinto da Silva on 01/11/22.
+//
+
+import SwiftUI
+
+struct OptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        OptionView()
+    }
+}
