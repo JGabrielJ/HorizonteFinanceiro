099e9d282f553fbc5f141d60b863ce89334ab68b
João Gabriel <gjacinto0707@gmail.com>
2022-11-03 14:36:27 -0300

    Created ConfigView.swift

---
+++ HorizonteFinanceiro/ConfigView.swift
@@ -0,0 +1,21 @@
+//
+//  ConfigView.swift
+//  HorizonteFinanceiro
+//
+//  Created by João Gabriel Jacinto da Silva on 03/11/22.
+//
+
+import SwiftUI
+
+
+struct ConfigView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfigView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfigView()
+    }
+}
