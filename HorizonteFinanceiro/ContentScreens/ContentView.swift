493028cb067a6ff6f575896286e8e82221d824b4
João Gabriel <gjacinto0707@gmail.com>
2022-11-04 15:07:10 -0300

    Update ContentView.swift

--- HorizonteFinanceiro/ContentScreens/ContentView.swift
+++ HorizonteFinanceiro/ContentScreens/ContentView.swift
@@ -8,43 +8,102 @@
 import SwiftUI
 
 
-struct HomeView: View {
-    var body: some View {
-        Text("Tela Inicial")
-    }
-}
-
 struct ContentView: View {
+    @State var expl = ""
+    @State var dica = ""
+    @State var page = -1
+    @State var idea = false
+    @State var conts = ["Conteúdo 1", "Conteúdo 2", "Conteúdo 3"]
+
     var body: some View {
         NavigationView {
-            VStack(alignment: .center) {
+            VStack {
                 Text("Educação Financeira e Seus Aspectos")
                     .bold()
                     .font(.title)
-                    .frame(width: 400, height: 200, alignment: .center)
+                    .frame(width: 400, height: 200)
                     .multilineTextAlignment(.center)
                     .navigationBarTitleDisplayMode(.inline)
-                    
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(.white)
-                    .frame(width: 400, height: 400, alignment: .center)
-                    .overlay(
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(.white)
+                        .frame(width: 350, height: 350)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(.blue, lineWidth: 3)
+                        )
+
+                    Text(expl)
+                }
+
+                Spacer()
+                
+                HStack {
+                    Button {
+                        if idea {
+                            dica = ""
+                            idea = false
+                        } else {
+                            dica = "Essa é uma dica!"
+                            idea = true
+                        }
+                    } label: {
+                        Image(systemName: "lightbulb.fill")
+                             .resizable()
+                             .scaledToFit()
+                             .foregroundColor(.yellow)
+                             .frame(width: 64, height: 44)
+                    }
+
+                    ZStack {
                         RoundedRectangle(cornerRadius: 15)
-                            .stroke(.blue, lineWidth: 3)
-                    )
+                            .fill(.white)
+                            .frame(width: 275, height: 64)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(.blue, lineWidth: 3)
+                            )
+
+                        Text(dica)
+                    }
+                }
 
                 Spacer()
 
-                    .toolbar {
-                        ToolbarItem(placement: .bottomBar) {
-                            NavigationLink(destination: HomeView(), label: {
+                .toolbar {
+                    ToolbarItem(placement: .bottomBar) {
+                        HStack {
+                            Button {
+                                if page >= 1 {
+                                    page -= 1
+                                    expl = conts[page]
+                                }
+                            } label: {
+                                Image(systemName: "arrow.left.square")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .foregroundColor(.blue)
+                                    .frame(width: 48, height: 48)
+                            }
+
+                            Spacer()
+
+                            Button {
+                                if page < conts.count - 1 {
+                                    page += 1
+                                    expl = conts[page]
+                                }
+                            } label: {
                                 Image(systemName: "arrow.right.square")
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 48, height: 48, alignment: .center)
-                            })
+                                    .foregroundColor(.blue)
+                                    .frame(width: 48, height: 48)
+                            }
                         }
                     }
+                }
             }
         }
     }
